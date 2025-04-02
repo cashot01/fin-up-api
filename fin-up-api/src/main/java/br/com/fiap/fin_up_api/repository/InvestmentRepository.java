d1bbc1beaae58afeb5474b80dab98300ae442eca
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-04-02 12:55:11 -0300

    funUp api

--- fin-up-api/src/main/java/br/com/fiap/fin_up_api/repository/InvestmentRepository.java
+++ fin-up-api/src/main/java/br/com/fiap/fin_up_api/repository/InvestmentRepository.java
@@ -3,6 +3,6 @@ package br.com.fiap.fin_up_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.fin_up_api.model.Investment;
-
-public interface InvestmentRepository extends JpaRepository<Investment, long> {
+import java.lang.Long;
+public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 }
