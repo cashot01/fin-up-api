693ef482e4a29e769b497bdd0d8aa12d623b3ba7
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-05-14 19:43:21 -0300

    update TransactionController

--- fin-up-api/src/main/java/br/com/fiap/fin_up_api/controller/TransactionController.java
+++ fin-up-api/src/main/java/br/com/fiap/fin_up_api/controller/TransactionController.java
@@ -1,21 +1,20 @@
 package br.com.fiap.fin_up_api.controller;
 
-import br.com.fiap.fin_up_api.model.Transaction;
-import br.com.fiap.fin_up_api.repository.TransactionRepository;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
+import br.com.fiap.fin_up_api.model.Transaction;
+import br.com.fiap.fin_up_api.repository.TransactionRepository;
+import br.com.fiap.fin_up_api.specification.TransactionSpecification;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("transactions")
@@ -30,10 +29,9 @@ public class TransactionController {
 
     @GetMapping
     public Page<Transaction> index(TransactionFilter filter,
-                                   @PageableDefault(size = 10, sort = "date", direction = Sort.Direction.DESC) Pageable pageable) {
+                                   @PageableDefault(size = 10, sort = "date", direction = Direction.DESC) Pageable pageable) {
         var specification = TransactionSpecification.withFilters(filter);
         return repository.findAll(specification, pageable);
-    }
     }
 
 }
